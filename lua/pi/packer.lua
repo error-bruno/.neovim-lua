20617138d5e1b5f9b1976b14757c3eef4a72b803
Ara Bruno <ara.bruno@aurora.tech>
2024-03-23 15:11:56 +0000

    Add nvim-surrond

--- lua/pi/packer.lua
+++ lua/pi/packer.lua
@@ -34,6 +34,25 @@ return require('packer').startup(function(use)
 		  {'L3MON4D3/LuaSnip'},
 	  }
   }
+  use({
+    'kylechui/nvim-surround',
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+        -- Examples to reference:
+        --Old text                    Command         New text
+        ----------------------------------------------------------------------------------
+        --surr*ound_words             ysiw)           (surround_words)
+        --*make strings               ys$"            "make strings"
+        --[delete ar*ound me!]        ds]             delete around me!
+        --remove <b>HTML t*ags</b>    dst             remove HTML tags
+        --'change quot*es'            cs'"            "change quotes"
+        --<b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+        --delete(functi*on calls)     dsf             function calls
+      })
+    end
+  })
   use( 'mhartington/formatter.nvim' )
   use( 'github/copilot.vim' )
   use( 'preservim/nerdcommenter' )
