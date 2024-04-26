bdd74d437260808216a62f464df160ff63b3bfc7
Ara Bruno <ara.bruno@aurora.tech>
2024-04-26 02:08:10 +0000

    Use vim diagnostic commands

--- lua/pi/plugins/lsp.lua
+++ lua/pi/plugins/lsp.lua
@@ -39,12 +39,19 @@ return {
 				vim.lsp.buf.rename()
 			end, opts)
 
+			vim.keymap.set("n", "<leader>vd", function()
+				vim.diagnostic.open_float()
+			end, opts)
+			vim.keymap.set("n", "[d", function()
+				vim.diagnostic.goto_next()
+			end, opts)
+			vim.keymap.set("n", "]d", function()
+				vim.diagnostic.goto_prev()
+			end, opts)
+
 			-- Unsure what these do
 			--vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
 			--vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-			--vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-			--vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-			--vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 			--vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 		end)
 
