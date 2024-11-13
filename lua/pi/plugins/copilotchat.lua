263ed1944f76a60fb884e8650fbfb53b82bf9778
Ara Bruno <ara.bruno@aurora.tech>
2024-11-13 15:38:43 +0000

    Change model

--- lua/pi/plugins/copilotchat.lua
+++ lua/pi/plugins/copilotchat.lua
@@ -152,6 +152,7 @@ return {
 		require("copilot_cmp").setup()
 
 		local chat = require("CopilotChat")
+		opts.model = 'claude-3.5-sonnet'
 		local select = require("CopilotChat.select")
 		-- Use unnamed register for the selection
 		opts.selection = select.unnamed
